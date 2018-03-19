3900073880ed8a33f1b5292f5fa701eaf54925b8
t03jam8 <j.margrove.08@aberdeen.ac.uk>
2018-03-19 23:16:38 +0100

    basic blockchain

---
+++ index.js
@@ -0,0 +1,41 @@
+const SHA256 = require("crypto-js/sha256");
+
+class Block {
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+  calculateHash() {
+    return SHA256(
+      this.index + this.timestamp + JSON.stringify(this.data)
+    ).toString();
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+  createGenesisBlock() {
+    return new Block(0, "01/01/2018", "Genesis Block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.previousHash;
+    this.chain.push(newBlock);
+  }
+}
+
+let venusCoin = new Blockchain();
+venusCoin.addBlock(new Block(1, "02/01/2018", { amount: 400 }));
+venusCoin.addBlock(new Block(2, "03/01/2018", { amount: 500 }));
+
+console.log(JSON.stringify(venusCoin));
