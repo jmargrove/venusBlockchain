d96a1e6ec2b2d6d06b14c7b4d198c6736d14610c
t03jam8 <j.margrove.08@aberdeen.ac.uk>
2018-03-21 20:21:48 +0100

    feat: proof of work, so we have to have a certain number of zeros before the new hash

--- index.js
+++ index.js
@@ -7,17 +7,33 @@ class Block {
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
   calculateHash() {
     return SHA256(
-      this.index + this.timestamp + JSON.stringify(this.data)
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data) +
+        this.nonce
     ).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("block mined : ", this.hash);
   }
 }
 
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 4;
   }
   createGenesisBlock() {
     return new Block(0, "01/01/2018", "Genesis Block", "0");
@@ -29,13 +45,34 @@ class Blockchain {
 
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.previousHash;
+    newBlock.mineBlock(this.difficulty);
+    //newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+    return true;
   }
 }
 
 let venusCoin = new Blockchain();
+console.log("mining block 1...", venusCoin);
 venusCoin.addBlock(new Block(1, "02/01/2018", { amount: 400 }));
-venusCoin.addBlock(new Block(2, "03/01/2018", { amount: 500 }));
+console.log("mining block 2...", venusCoin);
 
-console.log(JSON.stringify(venusCoin));
+venusCoin.addBlock(new Block(2, "03/01/2018", { amount: 500 }));
+console.log("mining block 2...", venusCoin);
+
+venusCoin.addBlock(new Block(3, "04/01/2018", { amount: 600 }));
+
+// console.log(JSON.stringify(venusCoin));
